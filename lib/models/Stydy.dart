3b165da13124fb1675b1f1bf6744856eff7278b7
Akbar <akbar358888@gmail.com>
2022-10-12 10:20:43 +0500

    Initial commit

--- lib/models/Stydy.dart
+++ lib/models/Stydy.dart
@@ -1,12 +1,12 @@
 class Study {
-  String title;
-  String titledesc;
-  List<String> desc;
-  String work;
-  List<String> workdesc;
-  String practice;
-  List<String> practicedesc;
+  String? title = "";
+  String? titledesc = "";
+  List<String>? desc =[];
+  String? work = "";
+  List<String>? workdesc = [];
+  String? practice = "";
+  List<String>? practicedesc = [];
 
-  Study(this.title, this.titledesc, this.desc, this.work, this.workdesc,
-      this.practice, this.practicedesc);
+  Study({required this.title, required this.titledesc,required this.desc,required this.work,required this.workdesc,
+    required  this.practice,required this.practicedesc});
 }
